78843e71a8b201a29957e9c4cab2b7918b8b866e
Tango Mango <aliciacastagnino@comcast.net>
2022-07-23 22:06:48 -0400

    added variables in javascript

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var charLength = 8
+var charUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var charLowercase = "abcdefghijklmnopqrstuvwxyz"
+var charSpecial = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-  var char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
-  var passwordLength = 8;
-  var password = "";
-
-  for (var i = 0; i<= passwordLength; i++) {
-    var randomNumber = math.floor(Math.random() * Chars.length);
-    password += chars.substring(randomNumber, randomNumber +1);
-  }
   
-}
-
+  passwordText.value = password;
+  
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
