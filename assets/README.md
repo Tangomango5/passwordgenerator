709d364c87a80fc7609a3a49c85344803f1458cc
Tango Mango <aliciacastagnino@comcast.net>
2022-07-25 21:24:25 -0400

    finished comments in javascript and finished my readme page

--- assets/README.md
+++ assets/README.md
@@ -1,2 +1,8 @@
+Welcome to the Password Generator!
 
-![alt text](\passwordgenerator\assets\Passwordgenerator.jpg) 
+I found this homework very challenging but managed to make it work! You can have 8 to 10 characters, choose if you want to have uppercase or lowercase, and it will alert you if you didn't put a number in. 
+
+Down below I have a screenshot of the project. I hope I am improving and I made sure to add comments this time!
+
+
+![Password Generator](/Javascript%20Homework/passwordgenerator/assets/Passwordgenerator.jpg)
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Function that creates random values
+//Function that picks random variables for what the password will consist of
 function randomInt(min, max) {
   if (!max) {
     max = min
@@ -17,19 +17,19 @@ function getRandomItem(list) {
   return list[randomInt(list.length)]
 }
 
-//Function that creates the random password
+//Function that creates the random password and connect to the write password function
 function generatePassword() {
 
   var userInput = window.prompt("how long would you like your password to be?")
 
+  //parseInt turns strings into number values
   var passwordLength = parseInt(userInput)
 
-  //Sends a message to the user that they did not enter anything
+  //Sends a message to the user that they did not enter any numbers
   if (isNaN(passwordLength)) {
     window.alert ("That's not a number!")
     return
   }
-
 
   //Gives an alert telling the viewer that they put an incorrect number
   if  (passwordLength < 8 || passwordLength > 128) {
@@ -37,51 +37,62 @@ function generatePassword() {
     return
   }
 
+  //Variables that sends the messages to the user to pick what they would like in their password
   var userWantsNumbers = window.confirm("Would you like your password to include numbers?")
   var userWantsSymbols = window.confirm("Would you like your password to include symbols?")
   var userWantsLowercase = window.confirm("Would you like your password to include lowercase letters?")
   var userWantsUppercase = window.confirm("Would you like your password to include uppercase letters?")
 
+//List of variables that will be in the password
   var numberList = ["0","1","2","3","4","5","6","7","8","9"]
   var symbolList = ["!","@","#","$","%","^","&","*"]
   var lowercaseList = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
   var uppercaseList = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
+
   var optionsCart = []
 
+  //this is the function that increases the length of the password
   for (var i = 0; i < lowercaseList.length; i++){
     uppercaseList[i] = lowercaseList[i].toUpperCase()
   }
 
+  //this is for if the user wants numbers
   if (userWantsNumbers === true) {
     optionsCart.push(numberList)
   }
   
+  //if the user wants symbols
   if (userWantsSymbols === true) {
     optionsCart.push(symbolList)
   }
   
+  //if the user wants lowercase
   if (userWantsLowercase === true) {
     optionsCart.push(lowercaseList)
   }
 
+  //if the user wants uppercase
   if (userWantsUppercase === true) {
     optionsCart.push(uppercaseList)
   }
 
+  //if the user does not pick any options then it defaults to lowercase
   if (optionsCart.Length === 0) {
     optionsCart.push(lowercaseList)
   }
 
+  //this is where the password will be generated
   var generatedPassword = ""
 
+  //function for random characters
   for (var i = 0; i < passwordLength; i++) {
     var randomList = getRandomItem (optionsCart)
     var randomChar = getRandomItem (randomList)
     generatedPassword += randomChar
   }
 
-  console.log(generatedPassword)
+  //this returns the function to the generated password variable to generate the password
   return generatedPassword
 
 };
